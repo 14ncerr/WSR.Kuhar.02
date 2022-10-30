074b02f151d211ab9c445498c7703296e568ef51
14ncerr <coffeelancerr@gmail.com>
2022-10-30 10:26:10 +0300

    newcommit

--- Kuhar.Lopushok/obj/Debug/net6.0-windows/App.g.i.cs
+++ Kuhar.Lopushok/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Kuhar.Lopushok {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Kuhar.Lopushok {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public static void Main() {
             Kuhar.Lopushok.App app = new Kuhar.Lopushok.App();
             app.InitializeComponent();
